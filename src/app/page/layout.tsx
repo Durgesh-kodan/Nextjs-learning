96b9fc2f4fce76fd752a90ba6d3c1fff54504588
Durgesh <durgeshkodan101@gmail.com>
2024-02-19 01:15:14 +0530

    got

---
+++ src/app/page/layout.tsx
@@ -0,0 +1,12 @@
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <h2>Inner layout Item</h2>
+      {children}
+    </>
+  );
+}
---
+++ src/app/page/page.tsx
@@ -0,0 +1,10 @@
+
+export default function page () {
+    return (
+        <div>
+            aaja bhidle
+        </div>
+    )
+}
+
+
