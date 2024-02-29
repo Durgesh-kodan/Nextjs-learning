e102d51516cfabd52bff7e36c1df689fc53ac69e
Durgesh <durgeshkodan101@gmail.com>
2024-03-01 01:56:21 +0530

    huen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-2xl text-center">hunehun</h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h1 className="text-2xl text-center">hunehun</h1>
+    </main>
   );
 }
