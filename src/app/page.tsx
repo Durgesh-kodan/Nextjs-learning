fb5aef9d11beaf4e52c1f6d9ed4562252c3f764e
Durgesh <durgeshkodan101@gmail.com>
2024-03-11 02:51:06 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcommingWebinars from "@/components/UpcommingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
@@ -8,6 +12,10 @@ export default function Home() {
       <HeroSection/>
       <FeaturedCourses/>
       <WhyChooseUs/>
+      <MusicSchoolTestimonials/>
+      <UpcommingWebinars/>
+      <Instructors/>
+      <Footer/>
     </main>
   );
 }
