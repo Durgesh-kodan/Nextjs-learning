734bfa130d6b8872c7d6fa2bf9dd2ee99eeeb152
Durgesh <durgeshkodan101@gmail.com>
2024-03-26 21:29:45 +0530

    created

---
+++ crud/src/dbconfig/db.config.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+    try {
+        
+    } catch (error) {
+        console.log("database connection Error");
+        console.log(error)
+    }
+}
