f0539d506ce35649eb26ad80d4509596b120ae02
Durgesh <durgeshkodan101@gmail.com>
2024-03-27 23:31:20 +0530

    updated

--- crud/src/dbconfig/db.config.ts
+++ crud/src/dbconfig/db.config.ts
@@ -2,7 +2,16 @@ import mongoose from "mongoose";
 
 export async function connect() {
     try {
-        
+        mongoose.connect(process.env.MONGO_URL!)
+        const connection = mongoose.connection
+        connection.on('connected' , ()=>{
+            console.log('MongoDB connected')
+        })
+
+        connection.on('error' , (err)=>{
+            console.log('error while connecting mongoDB' + err)
+            process.exit()
+        })
     } catch (error) {
         console.log("database connection Error");
         console.log(error)
